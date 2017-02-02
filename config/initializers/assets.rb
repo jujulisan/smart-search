3d3661941c587428c5045e4b6ed592e3190a7583
Juliane Lima <jujulisan@gmail.com>
2017-02-02 15:46:44 -0200

    Fixed offenses in rubocop and first test configured

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( creative/manifest.js creative/manifest.css )
-Rails.application.config.assets.precompile += %w( home-page.js home-page.css admin-page.js admin-page.css)
+Rails.application.config.assets.precompile += %w(creative/manifest.js creative/manifest.css)
+Rails.application.config.assets.precompile += %w(home-page.js home-page.css admin-page.js admin-page.css)
